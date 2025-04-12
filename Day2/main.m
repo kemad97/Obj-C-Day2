bb5216e6137655d0a48a533a5ed50e36f0092ef6
kemad97 <kerolos.emad992@gmail.com>
2025-04-12 12:15:24 +0200

    Initial Commit

---
+++ Day2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Day2
+//
+//  Created by Kerolos on 12/04/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
